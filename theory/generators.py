60218e137aa9f25e550183ce9d604c08446e2dac
Genaro Almaraz <jg08950@gmail.com>
2020-09-13 15:55:18 -0500

    Generators

---
+++ theory/generators.py
@@ -0,0 +1,15 @@
+def my_generator_function():
+    a = 1
+
+    yield a # Partial return
+
+    a = 2
+
+    yield a
+
+my_generator = my_generator_function() # A generator is an easy way of creating an iterator
+print(next(my_generator))
+print(next(my_generator))
+print(next(my_generator)) # StopIteration error
+
+# A generator helps us to keep states
