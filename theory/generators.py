dc6fa4e11113fd74d44e3589208e39dcb1472fa8
Genaro Almaraz <jg08950@gmail.com>
2020-09-13 16:09:16 -0500

    Yield vs Return

--- theory/generators.py
+++ theory/generators.py
@@ -13,3 +13,5 @@ print(next(my_generator))
 print(next(my_generator)) # StopIteration error
 
 # A generator helps us to keep states
+
+# Good reference: https://www.geeksforgeeks.org/use-yield-keyword-instead-return-keyword-python/
