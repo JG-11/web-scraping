de1ce7389bd8b1e5208b052a4001054cbaa198eb
Genaro Almaraz <jg08950@gmail.com>
2020-09-19 16:46:21 -0500

    Spider: custom output settings

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,16 +1,25 @@
 import scrapy
 
+
 class QuotesScraper(scrapy.Spider):
-    name = 'quotes' # scrapy crawl quotes
+    name = 'quotes'  # scrapy crawl quotes
 
     start_urls = [
         'http://quotes.toscrape.com/'
     ]
 
+    # avoid typing explicitly the spider output storage file in the terminal
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
+
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
-        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
+        quotes = response.xpath(
+            '//span[@class="text" and @itemprop="text"]/text()').getall()
+        tags = response.xpath(
+            '//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
 
         # Generator
         # scrapy crawl quotes -o quotes.json
