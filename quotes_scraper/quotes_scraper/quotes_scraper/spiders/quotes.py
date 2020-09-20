8feb14ea35a04b21af6a0195515f3917bc8e9f60
Genaro Almaraz <jg08950@gmail.com>
2020-09-19 19:02:50 -0500

    Multiple callbacks

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -14,6 +14,17 @@ class QuotesScraper(scrapy.Spider):
         'FEED_FORMAT': 'json'
     }
 
+    def parse_only_quotes(self, response, **kwargs):
+        kwargs['quotes'].extend(response.xpath(
+            '//span[@class="text" and @itemprop="text"]/text()').getall())
+
+        next_page_button_link = response.xpath(
+            '//ul[@class="pager"]/li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs=kwargs)
+        else:
+            yield kwargs
+
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath(
@@ -24,15 +35,14 @@ class QuotesScraper(scrapy.Spider):
         # Generator
         # scrapy crawl quotes -o quotes.json
         # scrapy crawl quotes -o quotes.csv
-        yield {
-            'title': title,
-            'quotes': quotes,
-            'top_ten_tags': tags
-        }
 
         next_page_button_link = response.xpath(
             '//ul[@class="pager"]/li[@class="next"]/a/@href').get()
 
         # code the case where we are in the last page
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse)
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={
+                'title': title,
+                'quotes': quotes,
+                'top_ten_tags': tags
+            })
